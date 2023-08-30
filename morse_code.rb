e5006db534aa7300339bcd43f6c1034abc6bff63
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-08-30 14:09:43 -0400

    Add methods and morse code

---
+++ morse_code.rb
@@ -0,0 +1,17 @@
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  morse_word.split.map { |char| decode_char(char) }.join
+end
